e3316d7f33e548c3f96b03a9ada6cea48ddfb101
lou <louiscuny@gmail.com>
2012-11-25 20:24:37 +0100

    add more specs

--- test/spec/SpecHelper.js
+++ test/spec/SpecHelper.js
@@ -1,9 +1,16 @@
-beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
+  var originalSelect;
+  var switchyContainer;
+  var switchyBar;
+  var switchySlider;
+
+  beforeEach(function() {
+    $('<select id="original-select"></select>').appendTo('body');
+    for (var i=1; i <= 3; i++) {
+      $('<option value="value'+i+'">text'+i+'</option>').appendTo($("#original-select"));
+    };
+    originalSelect = $("#original-select");
   });
-});
+
+  afterEach(function () {
+    $("#original-select, .switchy-container").remove();
+  });
--- test/spec/SwitchySpec.js
+++ test/spec/SwitchySpec.js
@@ -1,20 +1,58 @@
 describe("Switchy", function() {
 
-  var select;
-
-  beforeEach(function() {
-    $('<select id="select"></select>').appendTo('body');
-    for (var i=1; i <= 3; i++) {
-      $('<option value="value'+i+'">text'+i+'</option>').appendTo($("#multi-select"));
-    };
-    select = $("#select");
-  });
-
   describe('init', function(){
-    it ('should be chainable', function(){
-      select.switchy().addClass('chainable');
-      expect(select.hasClass('chainable')).toBeTruthy();
-    });
-  });
 
+    it ('should be chainable', function(){
+      originalSelect.switchy().addClass('chainable');
+      expect(originalSelect.hasClass('chainable')).toBeTruthy();
+    });
+
+    describe('without options', function(){
+
+      beforeEach(function() {
+        originalSelect.switchy();
+        switchyContainer = originalSelect.next();
+        switchyBar = switchyContainer.children();
+        switchySlider = switchyBar.children();
+      });
+
+      it('should hide the original select', function(){
+        expect(originalSelect.css('position')).toBe('absolute');
+        expect(originalSelect.css('left')).toBe('-9999px');
+      });
+
+      it('should create a container', function(){
+        expect(switchyContainer).toBe('div.switchy-container');
+      });
+
+      it ('should create a bar', function(){
+        expect(switchyContainer).toContain('div.switchy-bar');
+      });
+
+      it ('should create a slider', function(){
+        expect(switchyContainer).toContain('div.switchy-slider');
+      });
+
+    });
+
+    describe('without a selected option', function(){
+      it ('should init the slider position to CSS value', function(){
+        expect(switchySlider.position().left).toBe(0);
+      });
+    });
+
+    describe('with a selected option', function(){
+      it ('should init the slider position to the right one', function(){
+        originalSelect.children('option').eq(1).prop('selected', true);
+        originalSelect.switchy();
+        switchyContainer = originalSelect.next();
+        switchyBar = switchyContainer.children();
+        switchySlider = switchyBar.children();
+
+        var sliderPosition = (switchyBar.innerWidth() / (originalSelect.children('option').length - 1)) - (switchySlider.outerWidth(true) / 2);
+        expect(switchySlider.position().left).toBe(sliderPosition);
+      });
+    });
+
+  });
 });
