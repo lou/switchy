d26edc9dfb899a09a5578b2a080abc983025e5ed
lou <louiscuny@gmail.com>
2012-11-25 13:30:46 +0100

    add demos and css

---
+++ demos/application.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  $('#switch-me').switchy();
+
+  $('.gender').on('click', function(){
+    $('#switch-me').val($(this).attr('gender'));
+    $('#switch-me').trigger('change');
+  });
+
+  $('#switch-me').on('change', function(){
+    var bgColor = '#ccb3dc';
+    if ($(this).val() == 'female'){
+      bgColor = '#ed7ab0';
+    } else if ($(this).val() == 'male'){
+      bgColor = '#7fcbea';
+    }
+    $('.switch-bar').animate({
+      backgroundColor: bgColor
+    });
+  });
+});
